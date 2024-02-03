febfc0ed0705cd3a6b144d04a04013b4403c9f22
robertpanvip <robertpanvip@163.com>
2024-02-03 21:52:22 +0800

    修改实现方式

--- espkg.config.ts
+++ espkg.config.ts
@@ -3,5 +3,5 @@ import {defineConfig} from "es-pkg";
 export default defineConfig({
     "es": "./es",
     "lib": "./lib",
-    "publishDir":"../handler-fragment-npm"
+    "publishDir":"../dom-node-npm"
 })
